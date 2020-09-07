d833178cc203b331b6f6ba16f1757199620db022
Lonyun Cui <longyun.cui88@gmail.com>
2020-09-07 11:56:27 +0800

    function

--- app/Repositories/MT/Admin/IndexRepository.php
+++ app/Repositories/MT/Admin/IndexRepository.php
@@ -67,7 +67,7 @@ class IndexRepository {
         $consumption_data[1]['data'] = $data2->keyBy('day');
 
 
-        $insufficient_clients = User::where('usergroup','Service')->where('fund_expense_daily','>',0)
+        $insufficient_clients = User::where(['userstatus'=>'正常','status'=>1,'usergroup'=>'Service'])->where('fund_expense_daily','>',0)
             ->whereRaw("fund_balance < (fund_expense_daily * 7)")->get();
 
 
